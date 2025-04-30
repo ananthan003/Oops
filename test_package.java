011d9bbfe66a701c6b71adffb8990b2953af615a
ananthan003 <ananthu.ananthan003@gmail.com>
2025-04-30 14:32:30 +0530

    Add files via upload

---
+++ test_package.java
@@ -0,0 +1,21 @@
+import Graphics.Area;
+public class test_package{
+public static void main(String[] args)
+{
+Area A = new Area();
+System.out.println("** computation of area of diff figures**");
+System.out.println("\n***Rectangle**");
+A.rectangle();
+
+System.out.println("\n***Triangle**");
+A.triangle();
+
+System.out.println("\n***SQUARE**");
+A.square();
+
+System.out.println("\n**CIRCLE**");
+A.circle();
+}
+}
+
+
